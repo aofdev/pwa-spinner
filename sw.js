3455889af71ff1b0f61788f6d4d5ce076ed7ab2e
aofdev <aof.dev@gmail.com>
2018-01-09 14:31:42 +0700

    init code

---
+++ sw.js
@@ -0,0 +1,72 @@
+importScripts('workbox-sw.prod.v2.1.2.js');
+
+/**
+ * DO NOT EDIT THE FILE MANIFEST ENTRY
+ *
+ * The method precache() does the following:
+ * 1. Cache URLs in the manifest to a local cache.
+ * 2. When a network request is made for any of these URLs the response
+ *    will ALWAYS comes from the cache, NEVER the network.
+ * 3. When the service worker changes ONLY assets with a revision change are
+ *    updated, old cache entries are left as is.
+ *
+ * By changing the file manifest manually, your users may end up not receiving
+ * new versions of files because the revision hasn't changed.
+ *
+ * Please use workbox-build or some other tool / approach to generate the file
+ * manifest which accounts for changes to local files and update the revision
+ * accordingly.
+ */
+const fileManifest = [
+  {
+    "url": "index.html",
+    "revision": "052747d9f6b058a773a0e953af8d017d"
+  },
+  {
+    "url": "spinner.svg",
+    "revision": "4dd33676727189314840e68676440ee1"
+  }
+];
+
+const workboxSW = new self.WorkboxSW();
+
+
+workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'google-fonts',
+}));
+
+workboxSW.router.registerRoute('https://bootswatch.com/4/lumen/bootstrap.min.css', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'bootstrap-css'
+}));
+
+
+workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'animate-css'
+}));
+
+workboxSW.router.registerRoute('https://cdn.rawgit.com/alexgibson/shake.js/master/shake.js', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'shake-js'
+}));
+
+workboxSW.router.registerRoute('https://code.jquery.com/jquery-3.2.1.slim.min.js', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'jquery-js'
+}));
+
+workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'popper-js'
+}));
+
+
+workboxSW.router.registerRoute('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'bootstrap-js'
+}));
+
+
+workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'wow-js'
+}));
+
+
+
+workboxSW.precache(fileManifest);
+
---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "**/*.{html,svg}"
+  ],
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "workbox-cli-config.js"
+  ]
+};
